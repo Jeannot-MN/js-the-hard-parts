5482acf74597e046829a0f44a1a9a12f928b3e71
JeannotMN <jeannot@ssquared.dev>
2022-08-03 12:14:16 +0200

    playing with prototype

---
+++ oop/new.js
@@ -0,0 +1,32 @@
+function User(name, score) {
+    this.name = name;
+    this.score = score;
+}
+
+//Static value just available on the function used as an object
+User.static = 3;
+
+////Static value just available all instance but only updated on the instance😐😑
+User.prototype.static = 4;
+
+User.prototype.increment = function() {
+    this.score++;
+}
+
+User.prototype.decrement = function() {
+    this.score--;
+}
+
+const user = new User("Jane", 23);
+user.increment();
+
+user.static = 8;
+console.log(user.static);
+
+const user2 = new User("John", 24);
+
+//Wait what!!! Why are function the same but not primitives(😅😅)
+console.log(user.static === user2.static);
+console.log(user2.increment === user.increment);
+
+console.log(User.prototype);
