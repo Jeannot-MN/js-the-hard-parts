f4ace317ce273a734624f3db80bf49f859a8a5f5
JeannotMN <jeannot@ssquared.dev>
2022-08-06 21:02:09 +0200

    map function from scratch

---
+++ functional-js/index.js
@@ -0,0 +1,16 @@
+function map(array, operation) {
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        result.push(operation(array[i]));
+    }
+
+    return result;
+}
+
+function multiplyBy2(num) {
+    return num * 2;
+}
+
+const array = [1, 2, 3, 4];
+console.log(map(array, multiplyBy2));
