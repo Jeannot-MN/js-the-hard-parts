221c55d337901d3dc89618b93f7ac904a97f637e
JeannotMN <jeannot@ssquared.dev>
2022-08-08 11:21:17 +0200

    reduce function from scratch

---
+++ functional-js/reduce.js
@@ -0,0 +1,17 @@
+function reduce(array, reducer, accumulator) {
+    for (let i = 0; i < array.length; i++) {
+        accumulator = reducer(accumulator, array[i]);
+    }
+
+    return accumulator;
+}
+
+function map(array, operation) {
+    return reduce(array, (prev, curr) => {
+        prev.push(operation(curr));
+        return prev;
+    }, [])
+}
+
+console.log(reduce([1, 2, 3], (prev, curr) => prev + curr, 0));
+console.log(map([1, 2, 3], (el) => 2 * el));
