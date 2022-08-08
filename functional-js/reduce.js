a9ccb123ee99a7789e16eb3e4e39225897b8d96d
JeannotMN <jeannot@ssquared.dev>
2022-08-08 15:54:53 +0200

    reduce from scratch

--- functional-js/reduce.js
+++ functional-js/reduce.js
@@ -13,10 +13,5 @@ function map(array, operation) {
     }, [])
 }
 
-function* generator() {
-    //This is a generator function whose execution context can be paused and carried on latter. That is a very powerful feature in the jS language.
-    //Place it for video intro
-}
-
 console.log(reduce([1, 2, 3], (prev, curr) => prev + curr, 0));
 console.log(map([1, 2, 3], (el) => 2 * el));
