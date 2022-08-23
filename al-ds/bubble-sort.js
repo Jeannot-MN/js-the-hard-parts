ec964f201196ab1efb303bdd2de82d372dd29220
JeannotMN <jeannot@ssquared.dev>
2022-08-23 22:57:34 +0200

    bubble-sort version 2

--- al-ds/bubble-sort.js
+++ al-ds/bubble-sort.js
@@ -15,4 +15,20 @@ function bubble_sort(array) {
     return array;
 }
 
+function bubble_sort2(array) {
+    let swipe_occured;
+    do {
+        swipe_occured = false;
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(i, i + 1, array);
+                swipe_occured = true;
+            }
+        }
+    } while (swipe_occured)
+
+    return array;
+}
+
 console.log(bubble_sort([3, 2, 0, 4, 6, 3]));
+console.log(bubble_sort2([3, 2, 0, 4, 6, 3]));
