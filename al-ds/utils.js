9d89a5f36f6f820026326f56c0a41565eec3cbb7
JeannotMN <jeannot@ssquared.dev>
2022-08-24 23:16:14 +0200

    Array utility

--- al-ds/utils.js
+++ al-ds/utils.js
@@ -1,3 +1,13 @@
 export function swap(i, j, array) {
     [array[i], array[j]] = [array[j], array[i]];
 }
+
+export function Array(size) {
+    this.size = size;
+    this.pos = 0;
+    this.data = [];
+}
+
+Array.prototype.insert = function(element) {
+    this.data[this.pos++] = element;
+}
