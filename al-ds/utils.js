73ae80bd43437742b1d138c3cf4a52b5d9012b09
JeannotMN <jeannot@ssquared.dev>
2022-08-24 23:22:03 +0200

    Array utility methods

--- al-ds/utils.js
+++ al-ds/utils.js
@@ -11,3 +11,20 @@ export function Array(size) {
 Array.prototype.insert = function(element) {
     this.data[this.pos++] = element;
 }
+
+Array.prototype.clear = function() {
+    for (let i = 0; i < this.size; i++) {
+        this.data[i] = 0;
+    }
+    this.pos = 0;
+}
+
+Array.prototype.randomize = function() {
+    for (let i = 0; i < this.size; i++) {
+        this.data[i] = Math.floor(Math.random() * (this.size + 1));
+    }
+}
+
+Array.prototype.pop = function() {
+
+}
