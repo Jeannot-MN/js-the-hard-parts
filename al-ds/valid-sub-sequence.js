6d12e08ca2d2ea0134214f468fd150725fb50553
JeannotMN <jeannot@ssquared.dev>
2022-08-26 11:21:53 +0200

    valid subsequence

--- al-ds/valid-sub-sequence.js
+++ al-ds/valid-sub-sequence.js
@@ -1,9 +1,8 @@
 function isValidSubsquence(array, sequence) {
     let sequencePtr = 0;
     for (let arrayPtr = 0; arrayPtr < array.length; arrayPtr++) {
-        if (sequencePtr === sequence.length) return true;
         if (sequence[sequencePtr] === array[arrayPtr]) sequencePtr++;
     }
 
-    return false;
+    return sequencePtr === sequence.length;
 }
