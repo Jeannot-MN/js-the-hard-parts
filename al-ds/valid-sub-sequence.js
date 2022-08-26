da913e86ba054cbb4b724753f88e6ce07a751912
JeannotMN <jeannot@ssquared.dev>
2022-08-26 11:20:49 +0200

    valid subsequence stop once we know the sequence is valid

--- al-ds/valid-sub-sequence.js
+++ al-ds/valid-sub-sequence.js
@@ -1,8 +1,9 @@
 function isValidSubsquence(array, sequence) {
     let sequencePtr = 0;
     for (let arrayPtr = 0; arrayPtr < array.length; arrayPtr++) {
+        if (sequencePtr === sequence.length) return true;
         if (sequence[sequencePtr] === array[arrayPtr]) sequencePtr++;
     }
 
-    return sequencePtr === sequence.length;
+    return false;
 }
