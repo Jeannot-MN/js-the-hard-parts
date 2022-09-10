361bd21d444b15390927200fe56590c0105ba7ea
JeannotMN <jeannot@ssquared.dev>
2022-09-10 20:44:33 +0200

    playing with values and data types

---
+++ others/values.js
@@ -0,0 +1,21 @@
+var variable = null;
+
+console.log(variable);
+console.log(typeof variable);
+
+
+const a = 5;
+
+const b = "5a";
+
+console.log(a == b);
+console.log(a === b);
+
+const c = a / b;
+
+console.log(c);
+
+console.log(Number(b));
+
+const num = 9007199254740991;
+console.log(Number.MAX_SAFE_INTEGER + 1000);
