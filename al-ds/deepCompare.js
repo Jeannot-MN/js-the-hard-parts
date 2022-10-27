5fb52b4911d7723713f34bce5e2cbc59695ed350
JeannotMN <jeannot@ssquared.dev>
2022-10-27 21:23:42 +0200

    Array.prototype.deepCompare positive test case

--- al-ds/deepCompare.js
+++ al-ds/deepCompare.js
@@ -2,4 +2,8 @@ Array.prototype.deepCompare = function(array2) {
     return this.length === array2.length && this.every((el, i) => el === array2[i]);
 }
 
-function deepCompare(array)
+
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+
+console.log(a.deepCompare(b));
