4a0e259cd417324e5641cae58a5773b4bbd27b2d
JeannotMN <jeannot@ssquared.dev>
2022-10-29 23:37:54 +0200

    Array.prototype.deepCompare

--- functional-js/deepCompare.js
+++ functional-js/deepCompare.js
@@ -2,7 +2,6 @@ Array.prototype.deepCompare = function(array2) {
     return this.length === array2.length && this.every((el, i) => el === array2[i]);
 }
 
-
 const a = [1, 2, 3];
 let b = [1, 2, 3];
 console.log(a.deepCompare(b));
