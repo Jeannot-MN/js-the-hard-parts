01eaeec03ac07cd31d29262a2c633bc480d9c254
JeannotMN <jeannot@ssquared.dev>
2023-01-18 15:21:09 +0200

    node http module, req and res

--- node/http.js
+++ node/http.js
@@ -1,6 +1,13 @@
 import { createServer } from 'http';
+import net from 'net';
 
-const server = createServer((message) => {
+const server = createServer((req, res) => {
     console.log("A message just came in!");
+
+    // console.log(req, res);
+
+    res.write("Hello");
+    res.end();
 });
+
 server.listen(80);
